19339cab73e14590153bcdebb7be7a7d663d0a01
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-07-18 09:31:47 -0600

    pushing everything else

---
+++ src/main/java/com/docslilcoders/tacoslosprimos/TacosLosPrimosApplication.java
@@ -0,0 +1,13 @@
+package com.docslilcoders.tacoslosprimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosLosPrimosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacosLosPrimosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docslilcoders/tacoslosprimos/TacosLosPrimosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docslilcoders.tacoslosprimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacosLosPrimosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
