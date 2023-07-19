e8a32a7c4d790e1098d1ab2c90eef32abecbcefb
Jakira Dunson <jakiradunson@gmail.com>
2023-07-19 14:35:45 -0500

    api test

---
+++ src/main/java/com/docslilcoders/tacoslosprimos/models/StripePaymentResponse.java
@@ -0,0 +1,23 @@
+package com.docslilcoders.tacoslosprimos.models;
+
+import lombok.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class StripePaymentResponse {
+    private String clientSecret;
+}
+
+
+
+
+
+
+
+
+
+
+
+
