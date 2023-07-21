3de83d4125cf9adb7578a66310451202456ecc93
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-07-21 10:25:12 -0600

    loaded in menu

--- src/main/java/com/docslilcoders/tacoslosprimos/controllers/MenuController.java
+++ src/main/java/com/docslilcoders/tacoslosprimos/controllers/MenuController.java
@@ -1,14 +1,24 @@
 package com.docslilcoders.tacoslosprimos.controllers;
 
+import com.docslilcoders.tacoslosprimos.repositories.MenuItemRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MenuController {
 
+    private final MenuItemRepository menuItemDao;
+
+    public MenuController(MenuItemRepository menuItemDao) {
+        this.menuItemDao = menuItemDao;
+    }
+
+
     @GetMapping("/menu")
-    public String getAboutPage() {
+    public String getMenuPage(Model model) {
+        model.addAttribute("menuItems", menuItemDao.findAll());
         return "menu/menu";
     }
 
