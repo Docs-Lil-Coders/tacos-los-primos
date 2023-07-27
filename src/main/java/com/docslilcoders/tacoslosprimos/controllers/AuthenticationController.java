75c56f040241c1f2cb121e9f4a587be868c242f1
Jakira Dunson <jakiradunson@gmail.com>
2023-07-27 09:18:12 -0500

    edit page functinality

--- src/main/java/com/docslilcoders/tacoslosprimos/controllers/AuthenticationController.java
+++ src/main/java/com/docslilcoders/tacoslosprimos/controllers/AuthenticationController.java
@@ -1,12 +1,18 @@
 package com.docslilcoders.tacoslosprimos.controllers;
 
+import com.docslilcoders.tacoslosprimos.models.User;
+import com.docslilcoders.tacoslosprimos.services.AuthBuddy;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(Model model) {
+        User loggedInUser = AuthBuddy.getLoggedInUser();
+        model.addAttribute("loggedInUser", loggedInUser);
+
         return "users/login";
     }
 }
