904f1b24fcc2e9ea01594bfd75cec1b197fd410a
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-08-03 11:07:10 -0600

    developers now pull from database:

--- src/main/java/com/docslilcoders/tacoslosprimos/controllers/InfoController.java
+++ src/main/java/com/docslilcoders/tacoslosprimos/controllers/InfoController.java
@@ -1,10 +1,21 @@
 package com.docslilcoders.tacoslosprimos.controllers;
 
+import com.docslilcoders.tacoslosprimos.models.Developer;
+import com.docslilcoders.tacoslosprimos.repositories.DeveloperRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class InfoController {
+
+    private final DeveloperRepository developerDao;
+
+    public InfoController(DeveloperRepository developerDao){
+        this.developerDao = developerDao;
+    }
 
     @GetMapping("/about-us")
     public String getAboutPage() {
@@ -27,7 +38,28 @@ public class InfoController {
     }
 
     @GetMapping("/developers")
-    public String getDevelopersPage() {
+    public String getDevelopersPage(Model model) {
+        Optional<Developer> naime = developerDao.findById(1L);
+        if (naime.isEmpty()){
+            return "redirect:/login";
+        }
+        Optional<Developer> maddie = developerDao.findById(2L);
+        if (maddie.isEmpty()){
+            return "redirect:/login";
+        }
+        Optional<Developer> melissa = developerDao.findById(3L);
+        if (melissa.isEmpty()){
+            return "redirect:/login";
+        }
+        Optional<Developer> jakira = developerDao.findById(4L);
+        if (jakira.isEmpty()){
+            return "redirect:/login";
+        }
+
+        model.addAttribute("naime", naime.get());
+        model.addAttribute("maddie", maddie.get());
+        model.addAttribute("melissa", melissa.get());
+        model.addAttribute("jakira", jakira.get());
         return "info/developers";
     }
 
